ecdf81a894c12b07f93c89349a38fee82af0cc19
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2022-09-21 00:06:54 +0200

    Update serve.py

--- serve.py
+++ serve.py
@@ -12,20 +12,6 @@ logger = logging.getLogger(__name__)
 application = Flask(__name__)
 
 
-# Best Model Parameters
-mlflow_url = os.environ['MLFLOWSERVER_URL']
-ml_metric = 'metrics.Best_val_f1'
-ml_exp = '[Pipeline IMDB] Training'
-# Requesting 1 best model
-mlflow.set_tracking_uri(mlflow_url)
-mlflow.set_experiment(ml_exp)
-df = mlflow.search_runs()
-best_exp = df.loc[df[ml_metric].idxmax()]
-run_id = best_exp['run_id']
-print(run_id)
-print("Best Model:", run_id) ## test1
-logger.info("Best Model:"+str(run_id)) ##
-
 @application.route('/raw', methods=['POST'])
 def raw():
     # Read inputs/classes or Return reason of abort
@@ -34,6 +20,24 @@ def raw():
         abort(400)
     response = os.popen(request.json['command']).read().strip()
     logger.info('Visited raw page with bash command: \n%s'%request.json['command'])
+    
+    # Best Model Parameters
+    mlflow_url = os.environ['MLFLOWSERVER_URL']
+    ml_metric = 'metrics.Best_val_f1'
+    ml_exp = '[Pipeline IMDB] Training'
+    # Requesting 1 best model
+    mlflow.set_tracking_uri(mlflow_url)
+    mlflow.set_experiment(ml_exp)
+    df = mlflow.search_runs()
+    best_exp = df.loc[df[ml_metric].idxmax()]
+    run_id = best_exp['run_id']
+    print(run_id)
+    print("Best Model:", run_id) ## test1
+    logger.info("Best Model:"+str(run_id)) ##
+    logged_model = 'runs:/6ab063106f5648afafd37e53960c9349/pytorch-model'
+    loaded_model = mlflow.pytorch.load_model(logged_model).to(device)
+    logger.info(str(dir(loaded_model)))
+    
     return jsonify({'response': response}), 201
 
 
