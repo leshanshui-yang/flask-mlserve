a6274232f9ac66d6f233250535dc09e816f24345
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2022-09-20 23:44:21 +0200

    Update serve.py

--- serve.py
+++ serve.py
@@ -4,12 +4,27 @@ import subprocess
 import requests
 from flask import Flask, jsonify, request
 from flask import current_app, abort, Response
+import mlflow
 
 
 logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 application = Flask(__name__)
 
+
+# Best Model Parameters
+mlflow_url = os.environ['MLFLOWSERVER_URL']
+ml_metric = 'metrics.Best_val_f1'
+ml_exp = '[Pipeline IMDB] Training'
+# Requesting 1 best model
+mlflow.set_tracking_uri(mlflow_url)
+mlflow.set_experiment(ml_exp)
+df = mlflow.search_runs()
+best_exp = df.loc[df[ml_metric].idxmax()]
+run_id = best_exp['run_id']
+print(run_id)
+print("Best Model:", run_id) ## test1
+logger.info("Best Model:"+str(run_id)) ##
 
 @application.route('/raw', methods=['POST'])
 def raw():
@@ -25,7 +40,7 @@ def raw():
 @application.route('/')
 def index():
     logger.info('Visited index page')
-    return jsonify({'response': "index page"}), 201
+    return jsonify({'response': "index page"+str(run_id)}), 201
 
 
 if __name__ == '__main__':
